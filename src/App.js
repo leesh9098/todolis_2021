a26d8d9563171aeb55f02cf66a9fb215d997e745
leesh9098 <leesh3578@gmail.com>
2021-05-12 11:37:04 +0800

    21.05.12

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
-import logo from './logo.svg';
+import React from 'react';
+import { TextField, Typography } from '@material-ui/core';
+import { KeyboardDatePicker, KeyboardTimePicker } from '@material-ui/pickers';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      title: "",
+      content: "",
+      startDate: null,
+      startTime: null,
+      endDate: null,
+      endTime: null
+    }
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <div className="header">TODO LIST</div>
+        <div className="input_area">
+          <TextField label="제목" size="normal" margin="normal" fullWidth required />
+          <TextField label="상세내용" size="normal" margin="normal" fullWidth multiline />
+          <KeyboardDatePicker
+            disableToolbar
+            varient="inline"
+            format="yyyy/MM/DD"
+            margin="normal"
+            label="시작 예정일"
+            onChange={(value) => console.log(value)}
+            style={{ width: '50%' }}
+            KeyboardButtonProps={{
+              'aria-label': 'change date',
+            }}
+          />
+          <KeyboardTimePicker
+            margin="normal"
+            label="시작시간"
+            variant="inline"
+            onChange={(value) => console.log(value)}
+            style={{ width: '50%' }}
+            KeyboardButtonProps={{
+              'aria-label': 'change time',
+            }}
+          />
+          <TextField label="완료 예정일" type="datetime-local" InputLabelProps={{ shrink: true }} />
+        </div>
+        <div className="list_area">리스트 영역</div>
+        <Typography varient="body2" color="testSecondary" align="center">
+          {'Copyright ⓒ Leesh ' + new Date().getFullYear() + '.'}
+        </Typography>
+      </div>
+    );
+  }
 }
 
 export default App;
